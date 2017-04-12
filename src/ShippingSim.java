f4db5483e3ff89dd85fd17f4230d47ffda604853
CCOJ <randynguyen09@hotmail.com>
2017-04-12 11:06:56 -0500

    Add Factor of 10 to GenRate
    Updated project instructions:
    "UPDATE: The shipment per day values have increased by a factor of 10
    since the initial
    write-up. i.e. Minneapolis has 50 shipments per day now instead of 5."

--- src/ShippingSim.java
+++ src/ShippingSim.java
@@ -6,16 +6,16 @@ public class ShippingSim{
     static PQ agenda = new PQ();
 
     static Port[] portList = {
-            new Port(0,0,"Minneapolis",5),
-            new Port(0,10,"Saint Paul",5),
-            new Port(0,-6000,"Antarctica",1),
-            new Port(4000,4000,"Japan",10),
-            new Port(6000,5000,"Korea",5),
-            new Port(5000,6000,"China",100),
+            new Port(0,0,"Minneapolis",50),
+            new Port(0,10,"Saint Paul",50),
+            new Port(0,-6000,"Antarctica",10),
+            new Port(4000,4000,"Japan",100),
+            new Port(6000,5000,"Korea",50),
+            new Port(5000,6000,"China",1000),
             new Port(0,1000000,"Moon",0),
-            new Port(1000,3000,"Panama",5),
-            new Port(2000,2000,"Hawaii",5),
-            new Port(3000,3000,"Pirate Town",10)
+            new Port(1000,3000,"Panama",50),
+            new Port(2000,2000,"Hawaii",50),
+            new Port(3000,3000,"Pirate Town",100)
     };
 
     public static void main(String[] args){
