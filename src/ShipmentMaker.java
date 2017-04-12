2bc3d102f5096c934ce6092ccfa081ca298f9afb
CCOJ <randynguyen09@hotmail.com>
2017-04-12 11:50:34 -0500

    Changed variables names + docuemtnation in ShipmentMaker,java
    -Changed "islands" to ports where needed
    -Cleaned up documentation code
    
    **I believe we could do the moon's 0.1% generation chance in this. What
    do you think?**

--- src/ShipmentMaker.java
+++ src/ShipmentMaker.java
@@ -10,14 +10,14 @@ public class ShipmentMaker implements Event {
     }
 
     public void run() {
-        for (int i = 0; i < genRate; i++) { //Creates genRate number of packages per day
+        for (int i = 0; i < genRate; i++) { //Creates genRate number of packages per day based on the port
             int randNum = (int) (Math.random() * 9); //Randomly number between 0-8
-            int weight = (int) (Math.random() * 1000) + 1; //Weight of given package
+            int weight = (int) (Math.random() * 1000) + 1; //Random weight of given package
             double creationTime = ShippingSim.agenda.getCurrentTime(); //Time created
-            Port destinationIsland = ShippingSim.portList[(randNum + port.getPortNum() + 1) % 10]; //Get the random island
-            port.shipmentList[randNum].add(new Shipment(weight, creationTime, destinationIsland)); //Add the shipment to the designated island queue
+            Port destination = ShippingSim.portList[(randNum + port.getPortNum() + 1) % 10]; //Selects random port **This is most likely where we do the moon's 0.1% generation**
+            port.shipmentList[randNum].add(new Shipment(weight, creationTime, destination)); //Add the shipment to the designated port queue
 
         }
-        ShippingSim.agenda.add(this, 1);
+        ShippingSim.agenda.add(this, 1); //ShipmentMaker is added to the queue at a rate of 1 second
     }
 }
